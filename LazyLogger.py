83d0e4b98099c133dc7834fc26f85fa59cb81bed
felixhertrampf <hertrampf.f@gmail.com>
2019-07-26 11:08:01 +0200

    Added host name for logging on distributed services

--- LazyLogger.py
+++ LazyLogger.py
@@ -4,13 +4,15 @@ from os.path import basename
 
 
 class LazyLogger:
-	logger: logging.Logger
+	__logger: logging.Logger
+	host_name: str
 
-	def __init__(self, name=None):
-		self.logger: logging.Logger = logging.getLogger(name)
+	def __init__(self, name: str = None, host_name: str = ""):
+		self.__logger: logging.Logger = logging.getLogger(name)
+		self.host_name = host_name + ": " if host_name else  ""
 
 	def get_logger(self):
-		return self.logger
+		return self.__logger
 
 	@staticmethod
 	def __gather_stack_info(stack):
@@ -28,34 +30,32 @@ class LazyLogger:
 
 		return calling_class, calling_method
 
-	@staticmethod
-	def msg_format(calling_class, calling_method, msg):
-		return f"{calling_class}.{calling_method}(): {msg}"
+	def msg_format(self, calling_class, calling_method, msg):
+		return f"{self.host_name}{calling_class}.{calling_method}(): {msg}"
 
-	@staticmethod
-	def build_msg(stack, msg):
-		return LazyLogger.msg_format(*LazyLogger.__gather_stack_info(stack), msg)
+	def build_msg(self, stack, msg):
+		return LazyLogger.msg_format(self, *LazyLogger.__gather_stack_info(stack), msg)
 
 	def debug(self, msg, *args, **kwargs):
-		self.logger.debug(self.build_msg(inspect.stack(), msg), *args, **kwargs)
+		self.__logger.debug(self.build_msg(inspect.stack(), msg), *args, **kwargs)
 
 	def info(self, msg, *args, **kwargs):
-		self.logger.info(self.build_msg(inspect.stack(), msg), *args, **kwargs)
+		self.__logger.info(self.build_msg(inspect.stack(), msg), *args, **kwargs)
 
 	def warning(self, msg, *args, **kwargs):
-		self.logger.warning(self.build_msg(inspect.stack(), msg), *args, **kwargs)
+		self.__logger.warning(self.build_msg(inspect.stack(), msg), *args, **kwargs)
 
 	def warn(self, msg, *args, **kwargs):
-		self.logger.warn(self.build_msg(inspect.stack(), msg), *args, **kwargs)
+		self.__logger.warn(self.build_msg(inspect.stack(), msg), *args, **kwargs)
 
 	def error(self, msg, *args, **kwargs):
-		self.logger.error(self.build_msg(inspect.stack(), msg), *args, **kwargs)
+		self.__logger.error(self.build_msg(inspect.stack(), msg), *args, **kwargs)
 
 	def exception(self, msg, *args, exc_info=True, **kwargs):
-		self.logger.exception(self.build_msg(inspect.stack(), msg), *args, exc_info, **kwargs)
+		self.__logger.exception(self.build_msg(inspect.stack(), msg), *args, exc_info, **kwargs)
 
 	def critical(self, msg, *args, **kwargs):
-		self.logger.critical(self.build_msg(inspect.stack(), msg), *args, **kwargs)
+		self.__logger.critical(self.build_msg(inspect.stack(), msg), *args, **kwargs)
 
 	def log(self, level, msg, *args, **kwargs):
-		self.logger.log(level, self.build_msg(inspect.stack(), msg), *args, **kwargs)
+		self.__logger.log(level, self.build_msg(inspect.stack(), msg), *args, **kwargs)
